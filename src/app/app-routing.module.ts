d9b6d609fd6dfdf2b76fca46f83596d043275691
Anwesha Das <anwesha15586@mastek.com>
2022-06-11 15:57:08 +0530

    V2

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -5,13 +5,15 @@ import { ProjectListComponent } from './project-list/project-list.component';
 import { EmployeeComponent } from './employee/employee.component';
 import { EmployeeListComponent } from './employee-list/employee-list.component';
 import { AddEmployeeComponent } from './add-employee/add-employee.component';
+import { FeaturesComponent } from './features/features.component';
 
 const routes: Routes = [
   {path:"projects",component:ProjectListComponent},
   {path:"project-enquiry",component:ProjectEnquiryComponent},
   {path:"employee",component:EmployeeComponent},
   {path:"employee-list",component:EmployeeListComponent},
-  {path:"add-employee",component:AddEmployeeComponent}
+  {path:"add-employee",component:AddEmployeeComponent},
+  {path:"features",component:FeaturesComponent}
 ];
 
 @NgModule({
